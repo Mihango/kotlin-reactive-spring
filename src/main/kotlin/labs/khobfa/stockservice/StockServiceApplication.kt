69d63e8ee8a894d77c34dee4bfdec7d0402f53a9
Mihango <mihangokennedy@gmail.com>
2020-04-11 19:26:55 +0300

    project setup

---
+++ src/main/kotlin/labs/khobfa/stockservice/StockServiceApplication.kt
@@ -0,0 +1,11 @@
+package labs.khobfa.stockservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StockServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<StockServiceApplication>(*args)
+}
